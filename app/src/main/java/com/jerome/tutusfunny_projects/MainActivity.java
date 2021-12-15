df1a45ab11761a4eb4a2efdf810f1b90209903e9
unknown <zachariah11jerobert@gmail.com>
2021-12-15 06:41:06 +0530

    initial commit for main branch

---
+++ app/src/main/java/com/jerome/tutusfunny_projects/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jerome.tutusfunny_projects;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
